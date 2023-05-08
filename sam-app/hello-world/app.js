01d5b531a99d3ba34a4f3b156bbf71e894b2eafe
jaedeok_dev <jaedeok_dev@jake0208-PC.local>
2023-05-09 03:54:33 +0900

    feat: Add app logic

--- sam-app/hello-world/app.js
+++ sam-app/hello-world/app.js
@@ -1,21 +1,62 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
+const AWS = require("aws-sdk")
+AWS.config.update({ region: 'ap-northeast-2' })
 let response;
 
-exports.lambdaHandler = async (event, context) => {
+const db = new AWS.DynamoDB.DocumentClient()
+const TableName = process.env.TABLE_NAME
+console.log(TableName)
+
+exports.create = async (event, context) => {
+    let newData = null
+    const body = JSON.parse(event.body)
     try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello devops4',
-                // location: ret.data.trim()
-            })
+        try {
+            newData = {
+                ID: body.ID,
+                data: body.data
+            }
+        } catch (_) {
+            newData = {
+                ID: "test",
+                data: "please call with post method and approprate data format"
+            }
         }
+        console.log(newData)
+        await db.put({
+            TableName,
+            Item: newData,
+        })
+            .promise()
+
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+    response = { statusCode: 200, body: JSON.stringify(newData) }
+
+    return response
+};
+
+exports.list = async (event, context) => {
+    let datas = null
+    try {
+        datas = await db
+            .scan({
+                TableName,
+            })
+            .promise()
+
     } catch (err) {
         console.log(err);
         return err;
     }
 
+    try {
+        response = { statusCode: 200, body: JSON.stringify(datas) }
+    } catch (_) {
+        response = { statusCode: 200, body: "there is no data in dynamoDB" }
+    }
+
     return response
 };
+
